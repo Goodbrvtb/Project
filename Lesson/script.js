e556c24a3296ece141777b59fbe53ee8b10c8e80
Vova <petrovic_h@mail.ru>
2022-10-16 22:15:50 +0300

    2

---
+++ Lesson/script.js
@@ -0,0 +1,20 @@
+﻿"use strict";
+
+// console.log(typeof(Number('4')));
+
+// console.log(typeof(+'5'));
+
+// let answ = +prompt("Hello", "");
+
+
+let swither = null;
+
+if (swither) {
+    console.log('Working...');
+}
+
+swither = 1;
+
+if (swither) {
+    console.log('Working...');
+}
