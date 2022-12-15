518fc9ab482019abfe6555885a84f277e55de041
Vova <petrovic_h@mail.ru>
2022-12-15 22:39:26 +0300

    date & timer

---
+++ data/script.js
@@ -0,0 +1,25 @@
+'use strict';
+
+const now = new Date('2022-12-15');
+// new Date.parse('2020-05-01');
+console.log(now.setHours(18));
+console.log(now);
+
+// console.log(now.getFullYear());
+// console.log(now.getUTCMonth());
+// console.log(now.getDate());
+// console.log(now.getHours());
+// console.log(now.getUTCHours());
+
+// console.log(now.getTimezoneOffset());
+
+// console.log(now.getTime());
+
+let start = new Date();
+
+for (let i = 0; i < 100000; i++) {
+    let some = i ** 3;
+};
+
+let end = new Date();
+alert(`Цикл отработал за ${end - start} милисекунд`);
